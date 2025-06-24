15f47f258ed441337715f75e364442fb9ec99744
aaryanrpurohit <aaryanrpurohit@gmail.com>
2025-06-24 18:52:02 +0530

    feat: Initialize Password Manager project with React, Tailwind CSS, and Vite
    - Set up package.json with dependencies and scripts for development and production.
    - Create main application structure with App component, Navbar, and Manager components.
    - Implement password management functionality including saving, fetching, and deleting passwords.
    - Add styling with Tailwind CSS and custom CSS for a modern UI.
    - Integrate toast notifications for user feedback on actions.
    - Configure Vite for development and build processes.

---
+++ Password Manager/backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import passwordRoutes from './routes/passwords.js';
+
+dotenv.config();
+const app = express();
+
+app.use(cors({
+  origin: "https://web-projects-rgw873vje-aaryan-rajpurohits-projects.vercel.app",
+}));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected ✅"))
+  .catch((err) => console.error(err));
+
+// Routes
+app.use('/api/passwords', passwordRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
