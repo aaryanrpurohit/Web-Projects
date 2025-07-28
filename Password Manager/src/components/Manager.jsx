85b3aa98f417285d1f2f5b38ed8f9e9d56c5cbfc
Aaryan Rajpurohit <aaryanrpurohit@gmail.com>
2025-07-28 21:50:22 +0530

    Update Manager.jsx

--- Password Manager/src/components/Manager.jsx
+++ Password Manager/src/components/Manager.jsx
@@ -130,7 +130,7 @@ const Manager = () => {
                 required
               />
               <button
-                className='bg-green-600 px-4 py-2 rounded hover:bg-green-700 transition w-full mt-2'
+                className='bg-green-600 px-4 py-2 rounded hover:bg-green-700 transition pointer w-full mt-2'
                 onClick={savePassword}
               >
                 Save
