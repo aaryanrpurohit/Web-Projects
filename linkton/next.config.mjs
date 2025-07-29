b1e8ffcc1da84b9423967a8ed104cca2a898c9cc
Aaryan Rajpurohit <aaryanrpurohit@gmail.com>
2025-07-29 21:01:55 +0530

    Delete linkton directory

--- linkton/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['img.icons8.com'], // 👈 this is the domain causing the error
-  },
-};
-
-export default nextConfig;
