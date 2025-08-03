3237bb5f528cfe42186e5708b2f95d9ee6520163
aaryanrpurohit <aaryanrpurohit@gmail.com>
2025-08-03 22:51:35 +0530

    Back button

--- linktree/app/signup/page.js
+++ linktree/app/signup/page.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      
+    <div className='w-screen h-screen flex flex-col gap-10 justify-center items-center'>
+        <Link href="/">
+        <button className='p-10 bg-gray-400'>
+          --BACK--
+        </button>
+        </Link>
+      <p>
+        In Processing
+      GO BACK
+        </p>
     </div>
+
   )
 }
 
